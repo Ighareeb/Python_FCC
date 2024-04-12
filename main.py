8e17fbad10fd0ed13670ca0ecff8c2162d799f09
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-04-12 20:42:45 +0400

    # Tutorial 8:  Merge Sort Algo (Recursion)- Data Structures

--- main.py
+++ main.py
@@ -450,3 +450,54 @@ def move(n, source, auxiliary, target):
     print(A,B,C, '\n')
 # initiate call from source A to target C with auxiliary B
 print(NUMBER_OF_DISKS, A, B, C)
+
+# --------------------------------TUTORIAL 8---------------------------------------------------
+# Tutorial 8: Merge Sort Algo, (RECURSION) - Data Structures - see notes
+def merge_sort(array):
+    # BASE CASE: if array has 1 or 0 elements, it is already sorted
+    if len(array) <= 1:
+        return
+    # 1. Divide an unsorted sequence of items into sub-parts
+    middle_point = len(array) // 2
+    left_part = array[:middle_point]
+    right_part = array[middle_point:]
+    # 2. Sort the items in the sub-parts
+    merge_sort(left_part)
+    merge_sort(right_part)
+    # 3. Merge the sorted sub-parts
+    # variables will help keep track of each index during sorting process
+    left_array_index = 0
+    right_array_index = 0
+    sorted_index = 0
+    # COMPARE elements in left and right arrays and merge them in sorted order
+    while left_array_index < len(left_part) and right_array_index < len(right_part):
+        if left_part[left_array_index] < right_part[right_array_index]:
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+        else:
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+        sorted_index += 1
+        # another while loop to handle remaining elements in left and right arrays
+        while left_array_index < len(left_part):
+            array[sorted_index] = left_part[left_array_index]
+            left_array_index += 1
+            sorted_index += 1
+        while right_array_index < len(right_part):
+            array[sorted_index] = right_part[right_array_index]
+            right_array_index += 1
+            sorted_index += 1
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ')
+    print(numbers)
+    merge_sort(numbers)
+    print('Sorted array: ' + str(numbers))    
+    
+            
+            
+            
+            
+            
+            
+            
